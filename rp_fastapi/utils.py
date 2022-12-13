6c299afd0369cf312ddbdafc00f018b46f7c0efa
Nandha Kumar <nandha95@gmail.com>
2022-12-13 11:39:38 +0530

    Added base code.

---
+++ rp_fastapi/utils.py
@@ -0,0 +1,4 @@
+def get_user_id_generator():
+    n = 1
+    yield n
+    n += 1
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
-from rp_fastapi.main import app
+import random
+
 from fastapi.testclient import TestClient
+from rp_fastapi.app import app
 
 client = TestClient(app)
 
@@ -8,3 +10,18 @@ def test_get_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_get_item_id_works():
+    item_id = random.randint(1,10)
+    print(item_id)
+    response = client.get(f"/items/{item_id}")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": item_id}
+
+def test_get_item_id_fails():
+    item_id = 'abc'
+    print(item_id)
+    response = client.get(f"/items/{item_id}")
+    assert response.status_code == 422
+    # assert response.json() == {"item_id": item_id}
